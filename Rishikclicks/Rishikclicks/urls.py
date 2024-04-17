f3c17875ebad0d646d55d9097274f278e90c30ba
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2024-04-17 11:54:48 +0530

    cc

--- Rishikclicks/Rishikclicks/urls.py
+++ Rishikclicks/Rishikclicks/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Rishikapp.urls')),
 
-]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.UPLOAD_URL, document_root=settings.UPLOAD_URL)
+
